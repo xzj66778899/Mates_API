594de8956f9c3a08160e3690cc303c0f6f158ed6
xzj66778899 <xzj66778899@gmail.com>
2023-06-25 15:58:00 +1000

    add 'gender_hobbies' endpoint

--- blueprints/users_have_hobbies_bp.py
+++ blueprints/users_have_hobbies_bp.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, request, abort
+from flask import Blueprint, request, jsonify
 from models.user import User, UserSchema
 from models.hobby import Hobby, HobbySchema
+from models.gender import Gender
 from models.user_has_hobby import User_has_hobby, User_has_hobbySchema
 from init import db, bcrypt
 from sqlalchemy.exc import IntegrityError
@@ -52,7 +53,7 @@ def delect_user_has_hobby(user_has_hobby_id):
   
 
 
-# view users'hobbies'
+# view users'hobbies
 @users_have_hobbies_bp.route('/hobby_users/<int:h_id>')
 @jwt_required()
 def view_hobby_users(h_id):
@@ -67,3 +68,29 @@ def view_hobby_users(h_id):
       return User_has_hobbySchema(many = True, exclude = ['hobby_id']).dump(hobby_users_all)
    else:
       return{"message":'No one has this hobby yet.'}
+   
+
+
+# view gender's hobbies'
+@users_have_hobbies_bp.route('/hobby_genders/<int:g_id>')
+@jwt_required()
+def view_hobby_genders(g_id):
+   gender_exists = db.session.query(Gender.query.filter(Gender.id == g_id).exists()).scalar()
+   if not gender_exists:
+      return {"message":'This gender ID not exist.'}
+
+   stmt = db.select(User).filter_by(gender_id = g_id)
+   gender_users = db.session.execute(stmt).scalars().all()
+
+   if gender_users is not None:
+      hobbies = []
+      for user in gender_users:
+         hobby_stmt = db.select(User_has_hobby).filter_by(user_id = user.id)
+         gender_user_hobbies = db.session.execute(hobby_stmt).scalars().all()
+         for hobby_list in gender_user_hobbies:
+             hobbies.append(hobby_list.hobbies.name)
+      if hobbies == []:
+         return {"message":'This gender has no hobby yet.'}
+      return jsonify(list(hobbies))
+   else:
+      return{"message":'No user has this gender yet.'}
